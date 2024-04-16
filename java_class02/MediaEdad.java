ef11cc73298ef0937e8f6cb6cdc31cbee52a264a
bbotelho <brunnowr.bw@gmail.com>
2024-04-16 21:46:22 +0200

    automatic save home

--- java_class02/MediaEdad.java
+++ java_class02/MediaEdad.java
@@ -2,13 +2,20 @@ import java.util.Scanner;
 
 public class MediaEdad{
    public static void main(String[] args){
-		System.out.println("Que edad tiene tu padre?");
-		    Scanner edadPadres = new Scanner(System.in);
-		    int padre = edadPadres.nextInt();
-		    System.out.println("Que edad tiene tu madre?");
-		    int madre = edadPadres.nextInt();
-		    edadPadres.close();
-		    int mediaEdad = padre+madre;
-		    System.out.println("La media de Edad entre tus padres es: " + mediaEdad/2);
-	    }
-    }
+		  Scanner edadPadres = new Scanner(System.in);
+		  Scanner nombre = new Scanner(System.in);
+		  String nMadre, nPadre;
+		  System.out.println("Que edad tiene tu padre?");
+		  int padre = edadPadres.nextInt();
+			System.out.println("Cómo se llama tu padre ?");
+			nPadre = nombre.nextLine();
+			System.out.println("Que edad tiene tu madre?");
+			int madre = edadPadres.nextInt();
+			System.out.println("Cómo se llama tu madre ?");
+			nMadre = nombre.nextLine();
+			edadPadres.close();
+			nombre.close();
+			float mediaEdad = (padre+madre);
+			System.out.println("Tu Madre se llama " + nMadre + " e tiene " + madre + " años, e tu padre se llama " + nPadre + " e tiene " + padre + " años, La media de Edad entre tus padres es: " + mediaEdad/2);
+	  }
+}
