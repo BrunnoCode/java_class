35df6aad5001133e4509280e150af4fbb70caac6
bbotelho <brunnowr.bw@gmail.com>
2024-04-16 21:04:57 +0200

    automatic save home

--- java_class02/CuadradoDeI.java
+++ java_class02/CuadradoDeI.java
@@ -1,10 +1,10 @@
 
 public class CuadradoDeI {
-  public static void power(String[] args) {
+  public static void main(String[] args) {
     System.out.println("Iterar en orden descendiente de 20 al 10");
     for (int i = 20; i >= 10; i--){
       int cuadrado = i * 4;
-      System.out.println("cuadrado de " + i + " es" + cuadrado);
+      System.out.println("cuadrado de " + i + " es " + cuadrado);
     }
   }
 }
--- java_class02/MediaEdad.java
+++ java_class02/MediaEdad.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class MediaEdad{
-   public static void media(){
+   public static void main(String[] args){
 		System.out.println("Que edad tiene tu padre?");
 		    Scanner edadPadres = new Scanner(System.in);
 		    int padre = edadPadres.nextInt();
@@ -9,6 +9,6 @@ public class MediaEdad{
 		    int madre = edadPadres.nextInt();
 		    edadPadres.close();
 		    int mediaEdad = padre+madre;
-		    System.out.println("La media de Edad entre tus padres son: " + mediaEdad/2);
+		    System.out.println("La media de Edad entre tus padres es: " + mediaEdad/2);
 	    }
     }
