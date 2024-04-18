6635d1529d2ffb05658128314c34773f61531643
bbotelho <brunnowr.bw@gmail.com>
2024-04-18 18:26:28 +0200

    automatic save home

--- java_class03/Sonido.java
+++ java_class03/Sonido.java
@@ -6,7 +6,7 @@ public class Sonido {
   {
     Scanner scn = new Scanner(System.in);
     byte num;
-    String res;
+    boolean v = true;
   do{
       System.out.println("Selecione un numero y tendrás una representación de un sonido:");
       System.out.println("Selecione un numero de 1 - 5:");
@@ -15,30 +15,32 @@ public class Sonido {
       System.out.println("Gato");
       System.out.println("Pajaro");
       System.out.println("Burbujas de Agua!");
+      System.out.println("Salir presione 0!\n\n");
       num = scn.nextByte();
         switch (num) {
           case 1:
-            System.out.println("Au AU !!!");
+            System.out.println("Au AU !!!\n\n");
             break;
           case 2:
-            System.out.println("BOOOM !!!:");
+            System.out.println("BOOOM !!!:\n\n");
             break;
           case 3:
-            System.out.println("Miiiau!!!");
+            System.out.println("Miiiau!!!\n\n");
             break;
           case 4:
-            System.out.println("Piu Piu!!!");
+            System.out.println("Piu Piu!!!\n\n");
             break;
           case 5:
-            System.out.println("Blu Blu Blu !!!");
+            System.out.println("Blu Blu Blu !!!\n\n");
+            break;
+          case 0:
+            v = false;
             break;
           default:
-            System.out.println("Daaaahhhh este numero no te he pedido!!!");
+            System.out.println("Daaaahhhh este numero no te he pedido!!!\n\n");
             break;
         }
-        System.out.println("Quieres jugar de nuevo ?");
-        res = scn.nextLine();
-      } while(!res == "si");
+      } while(v);
       scn.close();
   }
 }
