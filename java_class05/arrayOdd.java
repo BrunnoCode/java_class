92ae394e098aafb0c6d4545de1ac7fa7ef50bf9b
bbotelho <brunnowr.bw@gmail.com>
2024-04-22 22:25:58 +0200

    automatic save home

--- java_class05/arrayOdd.java
+++ java_class05/arrayOdd.java
@@ -14,6 +14,40 @@ public class arrayOdd {
         System.out.println("otro numero");
       arrayUser[i] = scan.nextInt();
     }
-    
+    System.out.println("Array completado:");
+    for (int num : arrayUser){
+      System.out.print(num + ", ");
+    }
+    System.out.println("\nPresione qualquer digito para continuar.");
+    scan.nextInt();
+    System.out.println("Ahora vamos separarlo por impares y pares?, se si presione 1, no presione 0");
+    int res = scan.nextInt();
+    if (res == 1)
+    { 
+      int oddArray[] = {0};
+      int evenArray[] = {0};
+      int j = 0;
+      for (int i = 0; i < arrayUser.length; i++){
+        if (arrayUser[i] % 2 == 0)
+          evenArray[i] = arrayUser[i];
+        else
+          oddArray[j] = arrayUser[j++];
+      }
+      System.out.println("Array Original");
+      for (int index : arrayUser){
+        System.out.print(index + ", ");
+      }
+      System.out.println("Array de Pares");
+      for (int index : evenArray){
+        System.out.print(index + ", ");
+      }
+      System.out.println("Array de Impares");
+      for (int index : oddArray){
+        System.out.print(index + ", ");
+      }
+    }else {
+      System.out.println("Programa finalizado");
+      scan.close();
+    }
   }
 }
