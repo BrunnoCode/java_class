f89fe009fbf7e23f26e1a6eb3f24b984b3c6d0e0
bbotelho <brunnowr.bw@gmail.com>
2024-04-25 21:24:23 +0200

    automatic save home

---
+++ java_class07/fiesta.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class fiesta{
+  
+  public static void main(String[] args){
+   
+    ArrayList<String> listguests = new ArrayList<>(Arrays.asList("Michael", "Eva", "Ana", "Carlos", "Maria", "Laura", "Juan", "Guillem", "Anna", "Wen"));
+    writingStr("\nLa fiesta em mi casa será esta noche, y esta es mi lista oficial\n\n",0);
+    System.out.println("Lista de Invitados: "+listguests);
+    times(5000);
+    writingStr("\nMichael, que estaba el primero de la lista, resulta que está de viaje y no puede venir\n", 15);
+    listguests.remove(0);
+    System.out.print("OK, actualizando lista");
+    writingStr("......\n", 500);
+    System.out.println("Michael removido!");
+    System.out.println("Lista de Invitados Actualizada: "+listguests);
+    times(5000);
+    writingStr("\n\nTe llama la Pepi que se apunta con su novio Silvester\n\n", 50);
+    System.out.print("OK, actualizando lista");
+    writingStr("......\n", 500);
+    listguests.add("Pepi");
+    listguests.add("Silvester");
+    times(5000);
+    writingStr("\n\nTe encuentras a Eva, que dice que le cueles la tercera\n", 50);
+    System.out.print("OK, actualizando lista");
+    writingStr("......\n", 500);
+    
+    System.out.println("Lista de Invitados Actualizada: "+listguests);
+
+
+  }
+  public static void times(int t){
+      try{
+            Thread.sleep(t);
+    } catch(InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+  
+  
+  public static void writingStr(String str, int t){
+    try{
+      for (int i = 0; i < str.length(); i++){
+          System.out.print(str.charAt(i));
+          Thread.sleep(t);
+      }
+    } catch(InterruptedException e) {
+    e.printStackTrace();
+    }
+  }
+  
+}
