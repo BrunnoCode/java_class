d0db3f87b2146d6aa5222f1eabf6a017a7bac718
bbotelho <brunnowr.bw@gmail.com>
2024-04-29 17:37:53 +0200

    automatic save home

--- java_class07/fiesta.java
+++ java_class07/fiesta.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.Arrays;
 
 public class fiesta{
   
@@ -35,7 +36,28 @@ public class fiesta{
       System.out.println("Lista de Invitados Actualizada: "+listguests);
     } else
         System.out.println("\nOhh Eva no estaba en la lista, vaya que lío!");
-
+    times(5000);
+    writingStr("\nHay 10 personas más que vienen de la fiesta del vecino, debido a que nuestra musica mola bastante!\n", 80);
+    ArrayList<String> guestList = new ArrayList<>(Arrays.asList("Lucas, Vanessa, Adriana, Manuel, José Luis, Leonis, Layane, Geovanna, Pierre, Julio"));
+    System.out.println();
+    System.out.println("Estos son los recién llegados de la fiesta al lado, vamos añadirles:");
+    System.out.println(guestList);
+    System.out.print("OK, actualizando lista y ordenando en orden Alfabetica");
+    writingStr("......\n", 500);
+    times(3000);
+    listguests.addAll(guestList);
+    Collections.sort(listguests);
+    System.out.println("\nLista de Invitados Actualizada: "+listguests);
+    writingStr("Ultimo de la lista: " + (listguests.size()-1), 25);
+    System.out.println();
+    writingStr("No se ha olvidado de tu amigo Pedro verdad? \n\nVamos a ver si está en la Lista!.", 80);
+    times(3000);
+    System.out.println("Buscando a Pedro en la lista");
+    writingStr(".........", 80);
+    if(listguests.contains("Pedro"))
+      System.out.println("Sí, Pedro está en la lista!");
+    else
+      System.out.println("Pedro no está en la lista!");
 
   }
   public static void times(int t){
